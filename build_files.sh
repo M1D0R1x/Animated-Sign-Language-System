93f4cb5093e64d90109838c03b967b773b3ba3f2
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-09 18:33:47 +0530

    debug deploy 20, 19 -> build error

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,6 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 #python3 -m nltk.downloader punkt averaged_perceptron_tagger stopwords wordnet
 python3 manage.py collectstatic --noinput --clear
+mkdir "public/static"
 mv staticfiles public/static
 echo "BUILD END"
