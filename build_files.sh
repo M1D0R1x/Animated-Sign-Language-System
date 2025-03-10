9c27167e3fc603470caa15049b88fed5ad943b7f
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-10 21:04:34 +0530

    debug deploy v2.4 before got build error

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "BUILD START"
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 mkdir -p public/static
 mv staticfiles/* public/static/
 echo "BUILD END"
