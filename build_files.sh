7c41bfd677bdf2a23eb8ae21ce6068ca1a0ad41b
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-10 21:02:27 +0530

    debug deploy v2.3 before got build error

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "BUILD START"
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-python3 -m nltk.downloader punkt averaged_perceptron_tagger wordnet
-python3 manage.py collectstatic --noinput --clear
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 mkdir -p public/static
 mv staticfiles/* public/static/
 echo "BUILD END"
