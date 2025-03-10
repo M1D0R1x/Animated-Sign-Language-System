4fc89c775763bb4ab398ffb8f3410f21daf31305
M1D0R1x <veerababusaviti21@gmail.com>
2025-03-10 21:01:20 +0530

    debug deploy v2.2 before got build error

--- animated_sign_language_system/settings.py
+++ animated_sign_language_system/settings.py
@@ -3,6 +3,7 @@ import dj_database_url
 from pathlib import Path
 import nltk
 from django.conf import settings
+import django.contrib.staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3k7=!d39#4@&5a6to&4==j(c^v0(vv91cj5+9e8+d4&+01jb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True# Set to False for production
+DEBUG = False# Set to False for production
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
